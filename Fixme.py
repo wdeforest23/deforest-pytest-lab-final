c014deefa5c0be14f898994e9644fe35e37e3470
Lucas <lucaswelch7@gmail.com>
2023-02-05 22:03:20 -0800

    final3

--- Fixme.py
+++ Fixme.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+
 def evens(n):
+    return list(filter(lambda x: x % 2 == 0, map(lambda x: x, range(n + 1))))
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
